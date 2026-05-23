7d9cd9a8197246a352273541a5cd12bd558833a7
Jonnyfeo <jonnyremoto@hotmail.com>
2026-05-23 16:59:06 -0500

    Avance inicial: rutas, onboarding y auth visual

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Onboarding } from './components/onboarding/onboarding';
+import { Auth } from './components/auth/auth';
+import { Home } from './components/home/home';
+import { Profile } from './components/profile/profile';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'onboarding', pathMatch: 'full' },
+  { path: 'onboarding', component: Onboarding },
+  { path: 'auth', component: Auth },
+  { path: 'home', component: Home },
+  { path: 'profile', component: Profile }
+];
