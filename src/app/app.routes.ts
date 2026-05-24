fabf7957f0cb5ad0493a6ac8cf50db1851c86e04
Jonnyfeo <jonnyremoto@hotmail.com>
2026-05-23 21:37:03 -0500

    Creacion visual web responsive de games

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Onboarding } from './components/onboarding/onboarding';
 import { Auth } from './components/auth/auth';
 import { Home } from './components/home/home';
 import { Profile } from './components/profile/profile';
+import { Games } from './components/games/games';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'onboarding', pathMatch: 'full' },
   { path: 'onboarding', component: Onboarding },
   { path: 'auth', component: Auth },
   { path: 'home', component: Home },
-  { path: 'profile', component: Profile }
+  { path: 'profile', component: Profile },
+  { path: 'games', component: Games }
 ];
---
+++ src/app/components/games/games.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-games',
+  imports: [RouterLink],
+  templateUrl: './games.html',
+  styleUrl: './games.css'
+})
+export class Games {
+
+}
