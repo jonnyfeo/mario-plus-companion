2ad6e9a366260f6e1c30698072c3d0f1dc31544a
Javigo Gomez Molina <javigoled@gmail.com>
2026-05-24 18:19:08 -0500

    Implementación inicial del módulo de Mundos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { Home } from './components/home/home';
 import { Profile } from './components/profile/profile';
 import { Games } from './components/games/games';
 import { Personajes } from './components/personajes/personajes';
+import { MundosComponent } from './components/mundos/mundos'; // 👈 Esto busca tu archivo 'mundos.ts'
 
 export const routes: Routes = [
   { path: '', redirectTo: 'onboarding', pathMatch: 'full' },
@@ -13,5 +14,6 @@ export const routes: Routes = [
   { path: 'home', component: Home },
   { path: 'profile', component: Profile },
   { path: 'games', component: Games },
-  { path: 'personajes', component: Personajes }
+  { path: 'personajes', component: Personajes },
+  { path: 'mundos', component: MundosComponent } // 👈 Esto activa tu pantalla de mundos
 ];
